8ff5814f6d60601b84dab0706efa17b9046b78d9
Habib <thiernohabibbah@gmail.com>
2019-11-21 14:06:32 +0100

    Debut des tests

--- projet7/ClientBibliotheque-1/src/test/java/com/opendevup/ClientBibliotheque1ApplicationTests.java
+++ projet7/ClientBibliotheque-1/src/test/java/com/opendevup/ClientBibliotheque1ApplicationTests.java
@@ -1,16 +1,49 @@
 package com.opendevup;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import client.BibliotequeVilleWS;
+import client.BibliothequeService;
+import client.IOException_Exception;
+import client.Livre;
+import client.Utilisateur;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ClientBibliotheque1ApplicationTests {
 
+	BibliothequeService livreS = new BibliothequeService();
+	BibliotequeVilleWS bib = livreS.getBibliotequeVilleWSPort();
+	
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testGetListLivre() throws IOException_Exception {
+		
+		List<Livre> listeLivre = bib.listedeslivres();
+		int taille = listeLivre.size();
+		int res = 60;
+		
+		assertEquals(res, taille);
+	}
+	
+	@Test
+	public void testGetListUser() throws IOException_Exception {
+		
+		List<Utilisateur> listeuser = bib.listUser();
+		int taille = listeuser.size();
+		int res = 3;
+		assertEquals(res, taille);
 	}
 
 }
